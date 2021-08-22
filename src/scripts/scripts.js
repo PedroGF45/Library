dd319e604f21948253879a08c5e0b942aac9bad9
Pedro Brito <pedrobfh@gmail.com>
2021-08-22 16:28:26 +0100

    Primitive funtcion to add books to an array library

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,9 +1,29 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+const addBookBtn = document.getElementById("addBook");
 
-function addBookToLibrary() {
-  // do stuff here
-}
+
+
+//the object constructor
+function book() {
+    this.series = series,
+    this.nameBook = nameBook,
+    this.pages = pages,
+    this.review = review
+};
+
+
+//add books by clicking on button
+addBookBtn.addEventListener("click", function addBookToLibrary() {
+    const book1 = Object.create(book);
+    book1.series = prompt("series");
+    book1.nameBook = prompt("nameBook");
+    book1.pages = prompt("pages");
+    book1.review = prompt("review");
+    myLibrary.push(book1);
+})
+
+
+
+
+
