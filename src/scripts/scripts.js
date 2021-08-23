fb37def574c63e0ece0747d737c675f08b249cfc
Pedro Brito <pedrobfh@gmail.com>
2021-08-23 15:56:45 +0100

    Get Values from form

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,8 +1,12 @@
 let myLibrary = [];
 
-const addBookBtn = document.getElementById("addBook");
+const addBookModal = document.getElementById("addBook");
 const modalDisplay = document.getElementById("modal");
 const body = document.querySelectorAll("body > *:not(#modal)");
+const addBookBtn = document.getElementById("addBooktoLibrary");
+
+
+
 
 
 //the object constructor
@@ -15,9 +19,20 @@ function book() {
 
 
 //add books by clicking on button
-addBookBtn.addEventListener("click", function addBookToLibrary() {
+addBookModal.addEventListener("click", function openModal() {
     modalDisplay.classList.toggle("grid");
-
+    
     //makes everything blurr less the modal
     body.forEach(body => body.classList.toggle("blur"));  
+})
+
+addBookBtn.addEventListener("click", function addBooktoLibrary() {
+    modalDisplay.classList.toggle("grid");
+    body.forEach(body => body.classList.toggle("blur"));
+
+    //variables from the form
+    const seriesValue = document.getElementById("inputSeries").value;
+    const titleValue = document.getElementById("inputTitle").value;
+    const pagesValue = document.getElementById("inputPages").value;
+    
 })
